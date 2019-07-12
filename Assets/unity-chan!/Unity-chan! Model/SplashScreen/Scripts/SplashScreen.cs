55b746627948a3de3e7af156d85ee326eb011054
胡朋 <xiaomo@xiaomo.info>
2019-07-12 21:40:28 +0900

    unity chan

---
+++ Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityChan
+{
+    [ExecuteInEditMode]
+    public class SplashScreen : MonoBehaviour
+    {
+        void NextLevel()
+        {
+            SceneManager.LoadScene(SceneManager.sceneCount + 1);
+        }
+    }
+}
